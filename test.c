01f213461fd6af6ec72faabdc8d27d68152756ca
upupup814 <j18991381217@163.com>
2024-07-14 18:55:57 +0800

    Create test.c

---
+++ test.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+  int a,b ,c d;
+
+  printf("please enter 3 run:\n");
+  sacnf("%d%d%d",&a,&b,&c)
+  printf("%d,%d,%d\n",a,b,c);
+  while(1);
+  return 0;
+
+  
+}
